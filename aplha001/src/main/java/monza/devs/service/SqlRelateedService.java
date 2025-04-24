fc1aa160b34ef2be2bac70287458d016e27cabb7
nithitsuki <nithilanr@gmail.com>
2025-04-24 23:35:38 +0530

    maybe stupid fix?

--- aplha001/src/main/java/monza/devs/service/SqlRelateedService.java
+++ aplha001/src/main/java/monza/devs/service/SqlRelateedService.java
@@ -27,10 +27,7 @@ public class SqlRelateedService {
             
             Generate a valid SINGLE LINE SQL query based on the following natural language request
             and just say the sql query without any explanation or additional text:
-
-            dont display their age, join_date, salary, gender unless the user explicitly asks for more columns.
-            Try to always list the name in the select statement.
-
+            
             Make sure the SQL statement is in one line without any newlines or line breaks.
             Just say the SQL query without any explanation or additional text.
             """.stripIndent() + naturalLanguageQuery + """
